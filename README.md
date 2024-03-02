1d489e1cb48e9449f1bc502e73c91ccc7b530cc0
Aleksander Gondek <gondekaleksander@hotmail.com>
2024-03-02 15:39:16 +0100

    chore: initial setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# bazel-toolchains-explained
+Repository complimentary to my blog post series explaining the toolchains in Bazel
---
+++ default.nix
@@ -0,0 +1,27 @@
+{localSystem ? builtins.currentSystem, ...} @ args: let
+  external_sources = import ./nix/sources.nix;
+
+  nixpkgs_import_args = {
+    inherit localSystem;
+    config = {};
+  };
+  nixpkgs = import external_sources.nixpkgs nixpkgs_import_args;
+
+  devShell = nixpkgs.mkShell {
+    name = "bazel-toolchains-explained-shell";
+
+    packages = with nixpkgs; [
+      alejandra
+      bazel_7
+      bazel-buildtools
+      cocogitto
+      git
+      helix
+      niv
+      statix
+    ];
+  };
+
+in {
+  inherit devShell nixpkgs;
+}
